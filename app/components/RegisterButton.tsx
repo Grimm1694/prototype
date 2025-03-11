c71ae803c9156650c301f19fe4c42d940dac2cf7
Sujan <work.sujan26@gmail.com>
2025-03-12 02:27:18 +0530

    changes in header and reg btn

--- app/components/RegisterButton.tsx
+++ app/components/RegisterButton.tsx
@@ -4,10 +4,10 @@ import { motion } from "framer-motion";
 
 const RegisterButton = () => {
   return (
-    <section id="register" className="py-20 bg-hackathon-dark-blue">
+    <section id="register" className="py-16 sm:py-20 bg-hackathon-dark-blue">
       <div className="container mx-auto px-4 text-center">
         <motion.h2
-          className="text-4xl font-bold mb-8 text-hackathon-light-pink uppercase"
+          className="text-2xl sm:text-3xl font-press-start mb-8 text-hackathon-light-pink uppercase"
           initial={{ opacity: 0, y: 50 }}
           whileInView={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.5 }}
@@ -16,7 +16,7 @@ const RegisterButton = () => {
           Ready to join the Cosmic Coding Crusade?
         </motion.h2>
         <motion.p
-          className="text-xl mb-8 text-hackathon-beige"
+          className="text-md sm:text-lg mb-8 text-hackathon-beige font-jetbrains"
           initial={{ opacity: 0, y: 50 }}
           whileInView={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.5, delay: 0.2 }}
@@ -29,7 +29,7 @@ const RegisterButton = () => {
           href="https://unstop.com/hackathons/colossus-2025"
           target="_blank"
           rel="noopener noreferrer"
-          className="inline-block bg-hackathon-purple text-black px-8 py-3 rounded-full text-lg font-bold hover:bg-hackathon-lavender transition-all duration-300 border-2 border-hackathon-light-pink"
+          className="inline-block bg-hackathon-darker-blue text-hackathon-light-pink px-6 sm:px-8 py-2 sm:py-3 rounded-full text-sm font-jetbrains hover:bg-hackathon-lavender transition-all duration-300 border-2 border-hackathon-light-pink hover:text-black"
           whileHover={{ scale: 1.05 }}
           whileTap={{ scale: 0.95 }}
         >
